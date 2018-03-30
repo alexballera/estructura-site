650527b59cb07f90d155605e9c2982b68088e184
Alex Ballera <alexballera@gmail.com>
2018-03-30 19:43:04 -0400

    addHeadMeta

--- src/components/layout/index.js
+++ src/components/layout/index.js
@@ -2,8 +2,10 @@
 import createNavbar from './createNavbar'
 import createMain from './createMain'
 import createFooter from './createFooter'
+import addMeta from './addHeadMeta'
 
 export default () => {
+  addMeta()
   createNavbar()
   createMain()
   createFooter()
