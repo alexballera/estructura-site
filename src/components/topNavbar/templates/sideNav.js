f3a79d3501782276ea5f820eea1edee3fa4ba128
Alex Ballera <alexballera@gmail.com>
2018-03-30 17:23:36 -0400

    Estilizado side-nav

--- src/components/topNavbar/templates/sideNav.js
+++ src/components/topNavbar/templates/sideNav.js
@@ -6,15 +6,15 @@ const sideNav = yo`
 <ul className="side-nav" id="mobile-demo">
   <li className="waves-effect waves-blue" id="link-seccion-sidenav"><a href=${NavItems.links.home}>${NavItems.items.home}</a></li>
   <li className="divider"></li>
-  <li><a href=${NavItems.links.link1}>${NavItems.items.item1}</a></li>
+  <li className="waves-effect waves-blue" ><a href=${NavItems.links.link1}>${NavItems.items.item1}</a></li>
   <li className="divider"></li>
-  <li><a href=${NavItems.links.link2}>${NavItems.items.item2}</a></li>
+  <li className="waves-effect waves-blue" ><a href=${NavItems.links.link2}>${NavItems.items.item2}</a></li>
   <li className="divider"></li>
-  <li><a href=${NavItems.links.link3}>${NavItems.items.item3}</a></li>
+  <li className="waves-effect waves-blue" ><a href=${NavItems.links.link3}>${NavItems.items.item3}</a></li>
   <li className="divider"></li>
-  <li><a href=${NavItems.links.link4}>${NavItems.items.item4}</a></li>
+  <li className="waves-effect waves-blue" ><a href=${NavItems.links.link4}>${NavItems.items.item4}</a></li>
   <li className="divider"></li>
-  <li><a href=${NavItems.links.link5}>${NavItems.items.item5}</a></li>
+  <li className="waves-effect waves-blue" ><a href=${NavItems.links.link5}>${NavItems.items.item5}</a></li>
   <li className="divider"></li>
 </ul>
 `
