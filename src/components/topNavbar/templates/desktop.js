e1558ca97156ba852f010d30fc7e02381d6d3b74
Alex Ballera <alexballera@gmail.com>
2018-03-30 17:28:49 -0400

    Custom nabvar

--- src/components/topNavbar/templates/desktop.js
+++ src/components/topNavbar/templates/desktop.js
@@ -4,10 +4,10 @@ import {NavItems} from '../../navigation'
 
 const Desktop = yo`
 <ul className="navbar-top-desktop right hide-on-med-and-down">
-  <li id="li-${NavItems.id.home}"><a className="waves-effect waves-custom" href=${NavItems.links.home} >${NavItems.items.home}</a></li>
-  <li id="li-${NavItems.id.id1}"><a className="waves-effect waves-custom" href=${NavItems.links.link1} >${NavItems.items.item1}</a></li>
-  <li id="li-${NavItems.id.id2}"><a className="waves-effect waves-custom" href=${NavItems.links.link2}>${NavItems.items.item2}</a></li>
-  <li id="li-${NavItems.id.id3}"><a className="waves-effect waves-custom" href=${NavItems.links.link3}>${NavItems.items.item3}</a></li>
+  <li className="waves-effect waves-custom" id="li-${NavItems.id.home}"><a href=${NavItems.links.home} >${NavItems.items.home}</a></li>
+  <li className="waves-effect waves-custom" id="li-${NavItems.id.id1}"><a href=${NavItems.links.link1} >${NavItems.items.item1}</a></li>
+  <li className="waves-effect waves-custom" id="li-${NavItems.id.id2}"><a href=${NavItems.links.link2}>${NavItems.items.item2}</a></li>
+  <li className="waves-effect waves-custom" id="li-${NavItems.id.id3}"><a href=${NavItems.links.link3}>${NavItems.items.item3}</a></li>
 </ul>
 `
 const DesktopDropdown = yo`
